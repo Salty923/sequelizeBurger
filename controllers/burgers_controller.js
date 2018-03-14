eaee4fb4b127bf2793082e51325674dc58d6e738
Scott Saltzman <salty923@gmail.com>
2018-03-13 19:56:14 -0500

    Mark Slack Copy

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,42 @@
 var express = require("express");
-var burger = require('../models/burger.js');
+
 var router = express.Router();
+var burger = require("../models/burger");
 
-//var burger = require("../models/bugers.js");
+// get route -> index
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
 
-router.get("/",function (req, res) {
-    burger.selectAll(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-   // console.log(hbsObject);
+router.get("/burgers", function(req, res) {
+  // express callback response by calling burger.selectAllBurger
+  burger.all(function(data) {
+    // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+    var hbsObject = { burgers: data };
     res.render("index", hbsObject);
   });
 });
 
-
-router.post("/api/burgers", function(req, res) {
-  burger.insertOne(req.body.burger_name,
-    function(result) {
-    // Send back the ID of the new quote
-    alert(result);
-    res.json({ id: result.insertId });
+// post route -> back to index
+router.post("/burgers/create", function(req, res) {
+  // takes the request object using it as input for buger.addBurger
+  burger.create(req.body.burger_name, function(result) {
+    // wrapper for orm.js that using MySQL insert callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    res.redirect("/");
   });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-        var updateId = req.params.id;
-        burger.updateOne(updateId, function (result) {
-                if (result.changedRows == 0) {
-                        // If no rows were changed, then the ID must not exist, so 404
-                        return res.status(404).end();
-                } else {
-                        res.status(200).end();
-                }
-        });
+// put route -> back to index
+router.put("/burgers/update/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
+    // wrapper for orm.js that using MySQL update callback will return a log to console,
+    // render back to index with handle
+    console.log(result);
+    // Send back response and let page reload from .done in Ajax
+    res.json("/");
+  });
 });
 
-
-
 module.exports = router;
-
-
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
-INSERT INTO burgers(burger_name, devoured)
-VALUES ('BigMac',false),('Whopper',false),('Slider',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
